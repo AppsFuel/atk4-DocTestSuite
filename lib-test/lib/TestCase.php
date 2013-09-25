b1851949a8e7571a1dbc112dcc12c4f2ccdadbe1
Tommaso Allevi <tomallevi@gmail.com>
2013-09-25 16:09:36 +0200

    Publish protected assert methods

--- lib-test/lib/TestCase.php
+++ lib-test/lib/TestCase.php
@@ -18,7 +18,7 @@ class TestCase extends AbstractObject {
     	return $this->add("Factory_$factory_name")->$factory_method($attributes);
     }
 
-    protected function assertThrowException($exceptionType, $obj, $method, $args=array()) {
+    function assertThrowException($exceptionType, $obj, $method, $args=array()) {
         try {
 			//echo "pre call_user_func_array(array($obj, $method), $args) ";
             call_user_func_array(array($obj, $method), $args);
@@ -31,12 +31,12 @@ class TestCase extends AbstractObject {
         }
     }
 
-    protected function assertNotEmpty($data) {
+    function assertNotEmpty($data) {
         if (empty($data))
             $this->fails('Found data empty');
     }
 
-	protected function assertEmpty($data) {
+	function assertEmpty($data) {
         if (!empty($data))
             $this->fails('Found data not empty');
     }
@@ -45,27 +45,27 @@ class TestCase extends AbstractObject {
         print $msg;
     }
 
-    protected function assertEquals($expected, $actual) {
+    function assertEquals($expected, $actual) {
         if ($expected !== $actual)
             $this->fails("Expected: '" . print_r($expected, true) . "' given: '" . print_r($actual, true) . "'");
     }
 
-    protected function assertSame($expected, $actual) {
+    function assertSame($expected, $actual) {
         if ($expected != $actual)
             $this->fails("Expected: '" . print_r($expected, true) . "' given: '" . print_r($actual, true) . "'");
     }
 
-    protected function assertTrue($bool, $msg) {
+    function assertTrue($bool, $msg) {
         if ($bool !== true)
             $this->fails($msg);
     }
 
-    protected function assertFalse($bool, $msg) {
+    function assertFalse($bool, $msg) {
         if ($bool !== false)
             $this->fails($msg);
     }
     
-    protected function assertNotEquals($expected, $actual) {
+    function assertNotEquals($expected, $actual) {
         if ($expected === $actual)
             $this->fails("assertNotEquals: '" . print_r($expected, true) . "' given: '" . print_r($actual, true) . "'");
     }
